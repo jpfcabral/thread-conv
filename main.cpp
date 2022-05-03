302899f29c6c43270a1f767e6d2624d8490c3eaf
jpfcabral <jpfcabral@gmail.com>
2022-05-03 20:48:58 -0300

    add: kernel test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "utils/image.h"
+#include "utils/kernel.h"
 
 int main(){
   Image img("images/glassware_noisy.ascii.pgm");
   int **array = img.get_array();
-  for(int i = 0; i < img.get_numrows(); i++){
-    for(int j = 0; j < img.get_numcols(); j++){
-      cout << array[i][j] << " ";
-    }
-    cout << endl;
-  }
+
+  Kernel k(3,3);
+  k.print_kernel();
 }
