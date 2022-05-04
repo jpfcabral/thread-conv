51c79e859c6368fcd238a98214aaa27593bc7c15
jpfcabral <jpfcabral@gmail.com>
2022-05-04 10:58:59 -0300

    fix: read image

--- utils/image.cpp
+++ utils/image.cpp
@@ -9,19 +9,18 @@ Image::Image(string filename){
     int numcols = 0;
     int numrows = 0;
     int *shape = new int[2];
+    int max_value = 0;
 
     getline(file,line);
-
-    // Second line : comment
-    getline(file,line);
-    //cout << "Comment : " << line << endl;
 
     // Continue with a stringstream
     ss << file.rdbuf();
     // Third line : size
     ss >> numcols >> numrows;
-    //cout << numcols << " columns and " << numrows << " rows" << endl;
-    file.close();
+    cout << numcols << " columns and " << numrows << " rows" << endl;
+    
+    ss >> max_value;
+    cout << "Max value : " << max_value << endl;
 
     shape[0] = numcols;
     shape[1] = numrows;
@@ -33,6 +32,14 @@ Image::Image(string filename){
     for(int i = 0; i < numrows; i++){
         array[i] = (int*)malloc(numcols*sizeof(int));
     }
+
+    for(int row = 0; row < numrows; ++row){
+        for (int col = 0; col < numcols; ++col){
+            ss >> array[row][col];
+        }
+    }
+    
+    file.close();
 }
 
 Image::~Image(){
